b411bd88a1dc7e54e72dcf5195f0656119e9b1cd
sanskarb128-boop <sanskarb128@gmail.com>
2026-06-01 08:38:31 +0530

    Initial commit message

--- myflix/src/components/NavBar.jsx
+++ myflix/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                <Link to="/">MY FLIX</Link>
+                <Link to="/">NETFLIMIX</Link>
             </div>
 
             <div className="navbar-links">
